0e9ab7725fca2554058c9912fde0c48ff47e5fc9
RadicalIguana <d.lapsar16@gmail.com>
2023-04-11 11:34:16 +0500

    wip(database): implemention a database in progress

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,18 @@
-from fastapi import FastAPI
+from http.client import HTTPException
+import serial
+
+from fastapi import FastAPI, Depends, HTTPExcepton
 from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 
-# from receiver import accustripData
+from sqlalchemy.orm import Session
 
-import serial
+from .db import crud, models, schemas
+from .db.database import SessionLocal, engine 
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -19,6 +25,27 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail='Email already registered')
+    return crud.create_user(db=db, user=user)
+    
+@app.get('/users', response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_user(db, skip=skip, limit=limit)
+    return users
+
+# Create a database table...
 
 @app.get('/')
 async def read_root():
